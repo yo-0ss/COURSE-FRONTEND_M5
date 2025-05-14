489949630aae11f304ce7f1e2fa18895d41c1d34
yo-0ss <yoss.rios27@gmail.com>
2025-05-13 18:53:59 -0600

    Agrego ejercicio faltante del 04-29

---
+++ clase-04-29/app.js
@@ -0,0 +1,32 @@
+const orderList = document.getElementById('orderList');
+const addOrderBtn = document.getElementById('addOrderBtn');
+
+let orderId = 1; // Para identificar los pedidos
+
+addOrderBtn.addEventListener('click', () => {
+    const order = { id: orderId++, status: 'En Proceso' };
+    addOrder(order);
+    processOrder(order);
+});
+
+function addOrder(order) {
+    const listItem = document.createElement('li');
+    listItem.id = `order-${order.id}`;
+    listItem.textContent = `Pedido #${order.id}: ${order.status}`;
+    orderList.appendChild(listItem);
+}
+
+function updateOrderStatus(order, status) {
+    const listItem = document.getElementById(`order-${order.id}`);
+    if (listItem) {
+        listItem.textContent = `Pedido #${order.id}: ${status}`;
+    }
+}
+
+async function processOrder(order) {
+
+    const preparationTime = Math.floor(Math.random() * 5000) + 1000;
+    await new Promise((resolve, reject) => setTimeout(resolve, preparationTime));
+
+    updateOrderStatus(order, 'Completado');
+}
